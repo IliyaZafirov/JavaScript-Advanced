c31e8cb476b6e78ae35f7e2ddc56b746f0159bfb
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-01-14 08:23:10 +0200

    100/100

--- arrays-and-nested-arrays-lab/09-biggest-element.js
+++ arrays-and-nested-arrays-lab/09-biggest-element.js
@@ -1,6 +1,21 @@
-function solve() {
+// ⦁	Biggest Element
+// Write a function that finds the biggest element inside a matrix.
+// The input comes as an array of arrays, containing number elements (2D matrix of numbers).
+// The output is the return value of your function. Find the biggest element and return it.
 
+function solve(matrix) {
 
+    let result = [];
 
+    for (let i = 0; i < matrix.length; i++) {
+
+        for (let j = 0; j < matrix[i].length; j++) {
+            result.push(matrix[i][j]);
+        }
+    }
+    result.sort((a, b) => b - a);
+
+    return result[0];
 }
-solve()
+// solve([[20, 50, 10], [8, 33, 145]])
+solve([[3, 5, 7, 12], [-1, 4, 33, 2], [8, 3, 0, 4]])
