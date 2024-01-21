88ec79fbdb1f45f62c3d33c097b37bffc0ca1cf1
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-01-21 13:59:58 +0200

    100/100

--- syntax-functions-and-statements-lab/10-aggregate-elements.js
+++ syntax-functions-and-statements-lab/10-aggregate-elements.js
@@ -1,23 +1,17 @@
-// function solve(inputArr) {
+// ⦁	Aggregate Elements
+// Write a program that performs different operations on an array of elements. Implement the following operations:
+// ⦁	Sum(ai) - calculates the sum of all elements from the input array
+// ⦁	Sum(1/ai) - calculates the sum of the inverse values (1/ai) of all elements from the array
+// ⦁	Concat(ai) - concatenates the string representations of all elements from the array
+// The input comes as an array of number elements.
+// The output should be printed on the console on a new line for each of the operations.
 
-//     let sum = inputArr.reduce((acc, currVal) => acc + currVal)
-
-//     console.log(sum);
-
-//     let inverseSum = inputArr.reduce((acc, currVal) => acc + (1 / currVal))
-
-//     console.log(inverseSum);
-
-//     let concat = inputArr.join('');
-
-//     console.log(concat);
-
-// }
 
 function solve(elements) {
     aggregate(elements, 0, (a, b) => a + b);
     aggregate(elements, 0, (a, b) => a + 1 / b);
     aggregate(elements, '', (a, b) => a + b);
+
     function aggregate(arr, initVal, func) {
         let val = initVal;
         for (let i = 0; i < arr.length; i++)
