9239f52d8f9aeb27429da4634598b922d935b4ca
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-01-28 09:13:13 +0200

    100/100

---
+++ dom-manipulations-and-events-exercise/05. Encode-and-Decode-Messages/app.js
@@ -0,0 +1,34 @@
+function encodeAndDecodeMessages() {
+    const [encode, decode] = document.querySelectorAll('textarea');
+    const [encodeBtn, decodeBtn] = document.querySelectorAll('button');
+
+    encodeBtn.addEventListener('click', (e) => {
+        let charCodes = [];
+
+        for (let el of encode.value) {
+            charCodes.push(el.charCodeAt(0));
+        }
+
+        const result = charCodes.map(x => x += 1);
+        let output = '';
+        output = String.fromCharCode(...result);
+
+        decode.value = output;
+        encode.value = '';
+    });
+
+    decodeBtn.addEventListener('click', (e) => {
+        let codedChars = [];
+
+        for (let el of decode.value) {
+            codedChars.push(el.charCodeAt(0));
+        }
+
+        const result = codedChars.map(x => x -= 1);
+        let output = '';
+        output = String.fromCharCode(...result);
+
+        decode.value = output;
+        encode.value = '';
+    });
+}
