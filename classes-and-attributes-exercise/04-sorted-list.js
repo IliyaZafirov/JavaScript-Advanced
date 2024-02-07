ec94d3387a92bbf6e7d39416952d6a6394770dbe
Shaker <133087050+IliyaZafirov@users.noreply.github.com>
2024-02-07 08:11:07 +0200

    100/100

--- classes-and-attributes-exercise/04-sorted-list.js
+++ classes-and-attributes-exercise/04-sorted-list.js
@@ -1,25 +1,48 @@
+// ⦁	Sorted List
+// Implement a class List, which keeps a list of numbers, sorted in ascending order. It must support the following functionality:
+// ⦁	add(element) - adds a new element to the collection
+// ⦁	remove(index) - removes the element at position index
+// ⦁	get(index) - returns the value of the element at position index
+// ⦁	size - number of elements stored in the collection
+// The correct order of the elements must be kept at all times, regardless of which operation is called. Removing and retrieving elements shouldn’t work if the provided index points outside the length of the collection (either throw an error or do nothing). Note the size of the collection is not a function.
+// Input / Output
+// All functions that expect input will receive data as parameters. Functions that have validation will be tested with both valid and invalid data. Any result expected from a function should be returned as its result.
+// Your add and remove functions should return a class instance with the required functionality as its result. 
+// Submit the class definition as is, without wrapping it in any function.
+
+
 class List {
 
-
-    constructor(data) {
-        this.data = data
+    constructor(input) {
+        this.input = input;
+        this.list = [];
+        this.size = this.list.length;
     }
 
     add(el) {
-        this.data = this.data
+        this.size++;
+        this.list.push(el);
+        this.list.sort((a, b) => a - b);
+        return;
     }
 
     remove(idx) {
-
+        if (idx < 0 || idx > this.list.length - 1) {
+            throw new Error();
+        }
+        this.size--;
+        this.list.splice(idx, 1);
+        this.list.sort((a, b) => a - b);
+        return;
     }
 
     get(idx) {
-
+        if (idx < 0 || idx > this.list.length - 1) {
+            throw new Error();
+        }
+        return this.list[idx];
     }
 
-    size(size) {
-
-    }
 }
 
 let list = new List();
